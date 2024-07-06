ac851c3d0f2f75ce2722f8c4938d84009a324c83
MCS2721 <c.searles34@gmail.com>
2024-07-06 18:50:41 -0400

    Update learningTocode.js

--- learningTocode.js
+++ learningTocode.js
@@ -1,1 +1,16 @@
+var personName = "Christine";
+console.log(personName);
 
+var ID = "15A448D";
+var animal = "Squirrel";
+
+console.log("Name:", personName);
+console.log("ID:", ID);
+console.log("Animal of Choice:", animal);
+
+// This is a single line comment;
+
+/* this is a multi line comment
+see multi lines
+and more lines
+*/
